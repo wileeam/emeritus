bbd21c1845ec243c7cda21ba1cdc522e5e7865e5
Guillermo Rodriguez Cano <wileeam@users.noreply.github.com>
2021-06-04 20:38:25 +0200

    Initial gem skeleton

---
+++ lib/emeritus.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "emeritus/version"
+
+module Emeritus
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/emeritus_test.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class EmeritusTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Emeritus::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
